58774938c7dbcc07ccea25910c3f874d1f1d6727
Colocust <516100080@qq.com>
2021-06-08 22:31:28 +0800

    添加字典

--- structure/dict.go
+++ structure/dict.go
@@ -1,1 +1,26 @@
 package structure
+
+type dict struct {
+	table map[string]interface{}
+}
+
+func NewDict() *dict {
+	return &dict{
+		table: make(map[string]interface{}),
+	}
+}
+func (d *dict) Set(key string, value interface{}) {
+	d.table[key] = value
+}
+
+func (d *dict) Get(key string) interface{} {
+	return d.table[key]
+}
+
+func (d *dict) Remove(key string) {
+	delete(d.table, key)
+}
+
+func (d *dict) GetLen() int {
+	return len(d.table)
+}
--- structure/dict_test.go
+++ structure/dict_test.go
@@ -1,1 +1,19 @@
 package structure
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	d := make(map[string]int)
+
+	for i := 0; i < 10000000; i++ {
+		d[strconv.Itoa(i)] = i
+	}
+	fmt.Println("s")
+
+	delete(d,"1")
+	fmt.Println(len(d))
+}
