bedf8cac4163657f86020f8a08b4b7d17a186ced
Colocust <516100080@qq.com>
2021-06-20 21:22:34 +0800

    添加common文件，用于存放一些公共的结构体

---
+++ structure/common.go
@@ -0,0 +1,45 @@
+package structure
+
+type (
+	ZRangeSpec struct {
+		min, max     float32
+		minex, maxex bool //与最小值/最大值是否互斥（暂时理解为是否允许与最小值/最大值相同）
+	}
+)
+
+func NewZRangeSpec(min, max float32, minex, maxex bool) *ZRangeSpec {
+	return &ZRangeSpec{
+		min:   min,
+		max:   max,
+		minex: minex,
+		maxex: maxex,
+	}
+}
+
+func (zrs *ZRangeSpec) isValueGteMin(value float32) bool {
+	if zrs.minex {
+		if value > zrs.min {
+			return true
+		}
+		return false
+	} else {
+		if value >= zrs.min {
+			return true
+		}
+		return false
+	}
+}
+
+func (zrs *ZRangeSpec) isValueLteMax(value float32) bool {
+	if zrs.maxex {
+		if value < zrs.max {
+			return true
+		}
+		return false
+	} else {
+		if value <= zrs.max {
+			return true
+		}
+		return false
+	}
+}
