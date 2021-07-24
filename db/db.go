7090b374239fb00d2fb4815dbb633753cc5294f7
Colocust <516100080@qq.com>
2021-07-24 21:20:17 +0800

    添加db相关方法 待完善

--- db/db.go
+++ db/db.go
@@ -2,11 +2,13 @@ package db
 
 import (
 	"tinydb/object"
+	"tinydb/server"
 	"tinydb/structure"
 )
 
 type DB struct {
 	d *structure.Dict
+	e *structure.Dict
 }
 
 func NewDB() *DB {
@@ -15,7 +17,51 @@ func NewDB() *DB {
 	}
 }
 
-func (d *DB) LookupKeyRead(key string) *object.Object {
-	o := d.d.Get(key).(*object.Object)
-	return o
+func (db *DB) keyIsExpired(key string) bool {
+	return true
+}
+
+// 删除一个key 当它过期的时候
+func (db *DB) expireIfNeeded(key string) bool {
+	if !db.keyIsExpired(key) {
+		return false
+	}
+
+	server.StatExpiredKey++
+	// 同步/异步删除key (根据配置而定)
+
+	return true
+}
+
+func (db *DB) LookupKeyReadOrReply(key string) *object.Object {
+	return db.lookupKeyRead(key)
+}
+
+func (db *DB) lookupKeyRead(key string) *object.Object {
+	return db.lookupKeyReadWithFlags(key, server.LookupNone)
+}
+
+func (db *DB) lookupKeyReadWithFlags(key string, flag int) *object.Object {
+	if db.expireIfNeeded(key) {
+		server.StatMissesKey++
+	}
+	obj := db.lookupKey(key, flag)
+	if obj == nil {
+		server.StatMissesKey++
+	} else {
+		server.StatHitsKey++
+	}
+
+	return obj
+}
+
+func (db *DB) lookupKey(key string, flag int) *object.Object {
+	obj := db.d.Get(key).(*object.Object)
+	if obj != nil {
+		if flag == 1 {
+			// 补充LRU
+		}
+		return obj
+	}
+	return nil
 }
