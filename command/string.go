40aa3602a61064add8218af23fa26312a1ae8918
Colocust <516100080@qq.com>
2021-07-24 17:11:26 +0800

    修改命令所属文件

--- command/string.go
+++ command/string.go
@@ -1,7 +1,21 @@
 package command
 
-func Set() {
+import (
+	"tinydb/db"
+	"tinydb/object"
+)
 
+func GetCommand(d *db.DB, key string) *object.Object {
+	return getGenericCommand(d, key)
 }
 
-
+func getGenericCommand(d *db.DB, key string) *object.Object {
+	o := d.LookupKeyRead(key)
+	if o == nil {
+		return nil
+	}
+	if o.T != object.ObjString {
+		return nil
+	}
+	return o
+}
