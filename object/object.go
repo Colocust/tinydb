e2769e0e743292b8e2237c7832c6077d6c096917
Colocust <516100080@qq.com>
2021-07-24 14:48:14 +0800

    修改Object 指针名

--- object/object.go
+++ object/object.go
@@ -5,14 +5,15 @@ import "tinydb/structure"
 type Object struct {
 	T        uint8
 	Encoding uint8
-	P        interface{}
+	Ptr      interface {
+	}
 }
 
 func newObject(t, e uint8, p interface{}) *Object {
 	return &Object{
 		T:        t,
 		Encoding: e,
-		P:        p,
+		Ptr:      p,
 	}
 }
 
