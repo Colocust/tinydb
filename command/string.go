ef1c312d5022b3d08bb2e3896107840096d7725a
Colocust <516100080@qq.com>
2021-07-31 21:05:03 +0800

    添加set命令

--- command/string.go
+++ command/string.go
@@ -1,8 +1,18 @@
 package command
 
 import (
+	"time"
 	"tinydb/db"
 	"tinydb/object"
+	"tinydb/server"
+)
+
+const (
+	ObjSetNx   = 1 << 0
+	ObjSetXx   = 1 << 1
+	ObjSetEx   = 1 << 2
+	ObjSetPx   = 1 << 3
+	ObjKeepTTL = 1 << 4
 )
 
 func GetCommand(d *db.DB, key *object.Object) *object.Object {
@@ -20,6 +30,28 @@ func getGenericCommand(d *db.DB, key *object.Object) *object.Object {
 	return obj
 }
 
-func setGenericCommand(d *db.DB, flag int, key string, value string, expire int) {
+func SetCommand(d *db.DB) {
 
+}
+
+func setGenericCommand(d *db.DB, flag int, key *object.Object, value *object.Object, expire *object.Object) {
+	millisecond := 0
+	if expire != nil {
+		if expire.GetIntOrReply(&millisecond) != server.OK {
+			return
+		}
+		if millisecond <= 0 {
+			//addReply
+			return
+		}
+	}
+	if (flag == ObjSetNx && d.LookUpKeyWrite(key) != nil) || (flag == ObjSetXx && d.LookUpKeyWrite(key) == nil) {
+		return
+	}
+
+	d.SetKey(key, value, flag == ObjKeepTTL)
+	if expire != nil {
+		millisecond += time.Now().Second()
+		d.SetExpire(key, object.NewIntObject(&millisecond))
+	}
 }
