d7f77b826f650230f9bf72e0cd35e72274b34f45
Colocust <516100080@qq.com>
2021-08-14 16:42:02 +0800

    优化string命令

--- command/string.go
+++ command/string.go
@@ -3,7 +3,7 @@ package command
 import (
 	"time"
 	"tinydb/db"
-	"tinydb/enum"
+	"tinydb/errors"
 	"tinydb/object"
 )
 
@@ -15,41 +15,48 @@ const (
 	ObjKeepTTL = 1 << 4
 )
 
-func GetCommand(d *db.DB, key *object.Object) *object.Object {
+func GetCommand(d *db.DB, key *object.Object) (obj *object.Object, err error) {
 	return getGenericCommand(d, key)
 }
 
-func getGenericCommand(d *db.DB, key *object.Object) *object.Object {
-	obj := d.LookupKeyReadOrReply(key)
-	if obj == nil || obj.GetType() != object.ObjString {
-		return nil
+func getGenericCommand(d *db.DB, key *object.Object) (obj *object.Object, err error) {
+	obj = d.LookupKeyReadOrReply(key)
+
+	if obj.GetType() == object.ObjString {
+		return
 	}
-	return obj
+
+	err = errors.NewTypeError("it`s not a string type value")
+	return
 }
 
 func SetCommand(d *db.DB) {
 
 }
 
-func setGenericCommand(d *db.DB, flag int, key *object.Object, value *object.Object, expire *object.Object) {
-	millisecond := 0
-	if expire != nil {
-		if expire.GetIntOrReply(&millisecond) == enum.ERR {
-			return
-		}
-		if millisecond <= 0 {
-			//addReply
-			return
-		}
-	}
-	if (flag == ObjSetNx && d.LookUpKeyWrite(key) != nil) ||
-		(flag == ObjSetXx && d.LookUpKeyWrite(key) == nil) {
+func setGenericCommand(db *db.DB, flag int, key *object.Object, value *object.Object, expire *object.Object) (err error) {
+	if (flag == ObjSetNx && db.LookUpKeyWrite(key) != nil) || (flag == ObjSetXx && db.LookUpKeyWrite(key) == nil) {
 		return
 	}
 
-	d.SetKey(key, value, flag == ObjKeepTTL)
+	var ttl int
 	if expire != nil {
-		millisecond += time.Now().Second()
-		d.SetExpire(key, object.NewIntObject(&millisecond))
+		ttl, err = expire.GetIntValue()
+
+		if err != nil {
+			return
+		}
+		if ttl <= 0 {
+			err = errors.NewParameterError("invalid expire time")
+			return
+		}
+
+		ttl += time.Now().Second()
 	}
+
+	db.SetKey(key, value, flag == ObjKeepTTL)
+	if expire != nil {
+		db.SetExpire(key, object.NewIntObject(&ttl))
+	}
+	return
 }
