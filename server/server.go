4be9c5d409be5441e3ff23355b95d66e247de6bc
Colocust <516100080@qq.com>
2021-08-28 21:16:09 +0800

    添加Client结构体

--- server/server.go
+++ server/server.go
@@ -2,31 +2,34 @@ package server
 
 import (
 	"github.com/Colocust/strcture"
-	"os"
+	"net"
+	"tinydb/command"
 	"tinydb/config"
 	"tinydb/db"
+	"tinydb/object"
 )
 
 type (
 	Server struct {
-		Pid     int            // 进程号
-		Cfg     *config.Config // 配置
-		DB      *db.DB         // 全局数据库
-		Clients *strcture.List // 当前连接的客户端
+		Pid           int            // 进程号
+		Cfg           *config.Config // 配置
+		DB            *db.DB         // 全局数据库
+		Clients       *strcture.List // 当前连接的客户端
+		CurrentClient *Client        // 当前处理的客户端
+	}
+
+	Client struct {
+		Id      uint             // 唯一自增ID
+		Conn    *net.Conn        // 连接对象
+		DB      *db.DB           // 当前选择的数据库
+		Command *command.Command // 当前命令
+		Argc    int              // 当前命令的参数个数
+		Argv    object.Object    // 当前命令的参数
 	}
 )
 
-var serv *Server
+var Serv = new(Server)
 
-func InitServer(cfg *config.Config) {
-	serv = &Server{
-		Pid:     os.Getpid(),
-		Cfg:     cfg,
-		DB:      db.NewDB(),
-		Clients: strcture.NewList(),
-	}
-}
-
-func GetServ() *Server {
-	return serv
+func NewClient() *Client {
+	return new(Client)
 }
